f8eae5722a47141fbfc5aa503fe62e0e5aec2d7d
Alex <lxhaskins@gmail.com>
2025-11-01 17:26:55 -0700

    Updates styles.css, settings, and settings js

--- javascript/settings-script.js
+++ javascript/settings-script.js
@@ -1,3 +1,8 @@
 /**after db and authentication, whether the user is new or experienced
  * will display a different home page
  */
+
+const toggle = document.getElementById("theme-toggle");
+toggle.addEventListener("change", () => {
+  document.body.classList.toggle("dark-mode", toggle.checked);
+});
